beac5f2e2c64f8c6f5e468f21829f4ec7a3a5550
twedds95 <twedds95@hotmail.com>
2025-08-05 22:42:00 -0400

    catch more exceptions with qbitt

--- src/aaclient.py
+++ src/aaclient.py
@@ -128,40 +128,43 @@ class aaclient:
             username=self.qbitt_client["username"],
             password=self.qbitt_client["password"],
         )
-        
-        with open(t_path, "rb") as f:
-            torrent_data = f.read()
-        config = bdecode(torrent_data)
-        info = config["info"]
-        hash_bit = hashlib.sha1(bencode(info)).digest()
-        hash = hash_bit.hex()
-            
-        if len(info['files']) > 1500:
-            self.logger.error(f"This torrent has too much stuff, I don't want it. {t_path} not added to qBittorrent")
-            os.remove(t_path) 
-            return False
-                
-        qb = qbittorrentapi.Client(**conn_info)        
-        qb.torrents_add(torrent_files=t_path, category=self.qbitt_client["musicCategory"], is_paused=True)
-        time.sleep(1) # allow metadata to be downloaded
-        os.remove(t_path)
-        
-        files = qb.torrents_files(hash)    
-        qb.torrents_file_priority(hash, [i for i in range(len(files))], priority=0) # Do not download   
-        
+        is_succes = False
         try:
+            with open(t_path, "rb") as f:
+                torrent_data = f.read()
+            config = bdecode(torrent_data)
+            info = config["info"]
+            hash_bit = hashlib.sha1(bencode(info)).digest()
+            hash = hash_bit.hex()
+                
+            if len(info['files']) > 1500:
+                self.logger.error(f"This torrent has too much stuff, I don't want it. {t_path} not added to qBittorrent") 
+                is_succes = False
+                return is_succes
+                    
+            qb = qbittorrentapi.Client(**conn_info)        
+            qb.torrents_add(torrent_files=t_path, category=self.qbitt_client["musicCategory"], is_paused=True)
+            time.sleep(1) # allow metadata to be downloaded
+            
+            files = qb.torrents_files(hash)    
+            qb.torrents_file_priority(hash, [i for i in range(len(files))], priority=0) # Do not download   
+        
             idx = qbitt_file_search(files.data, desired_file)   
             qb.torrents_file_priority(hash, idx, 1) # Normal dl
             new_path = os.path.dirname(files[idx].name) + "/" +  save_filename
             qb.torrents_rename_file(hash, idx, new_path)
             qb.torrents_start(hash)        
             self.logger.info(f"{save_filename} added to qBittorrent")
-            return True       
+            is_succes = True       
         except:
             qb.torrents_delete(True, hash)        
-            self.logger.error(f"Error adding book. {t_path} removed from qBittorrent")  
+            self.logger.error(f"Error adding book. {t_path} removed from qBittorrent")
+            is_succes = False
+        finally:
+            os.remove(t_path) 
+            
         
-        return False
+        return is_succes
         
     def torrent_from_bookbounty(self, link, save_as, save_path):    
         path, fname, save_as = self.dl_torrent_from_listing(link, save_as)
